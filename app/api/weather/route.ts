887e1cab9bb2db72967f12d16168b3a090120cdd
Kawashima-Tomoya <104120078+Kawashima-Tomoya@users.noreply.github.com>
2025-01-15 18:19:06 +0900

    🔇

--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -8,15 +8,10 @@ export async function GET(req: Request) {
   const data = await res.json()
   const lat = data[0].lat
   const lon = data[0].lon
-  // console.log(lat, lon)
 
   const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly,daily,alerts&appid=${process.env.WEATHER_API_KEY}&lang=ja`
   const response = await fetch(url)
   const weather = await response.json()
-  // console.log(weather)
 
   return Response.json({ weather })
-  // return new Response(JSON.stringify({ weather }), {
-  //   headers: { 'Content-Type': 'application/json' },
-  // })
 }
