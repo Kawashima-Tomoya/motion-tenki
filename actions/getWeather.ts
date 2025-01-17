31b7418ba8bcb847540f6d623424ec4af6bf8765
Kawashima-Tomoya <104120078+Kawashima-Tomoya@users.noreply.github.com>
2025-01-17 17:51:51 +0900

    ✨ serverActionでOne Call API 3.0に変更

--- actions/getWeather.ts
+++ actions/getWeather.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use server'
 
 import process from 'node:process'
@@ -10,16 +11,22 @@ export async function getWeather(_prevState: any, formData: FormData) {
       weather: '都市名を入力してください',
     }
   }
-  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}`)
-  if (!res.ok) {
+
+  const resGeo = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${process.env.WEATHER_API_KEY}`)
+  const dataGeo = await resGeo.json()
+  const lat = dataGeo[0].lat
+  const lon = dataGeo[0].lon
+
+  const resWeather = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly,daily,alerts&appid=${process.env.WEATHER_API_KEY}&lang=ja`)
+  if (!resWeather.ok) {
     return {
       city,
       weather: '都市が見つかりません',
     }
   }
-
-  const data = await res.json()
-  const weather = data.weather[0].main
+  const dataWeather = await resWeather.json()
+  const weather = dataWeather.current.weather[0].description
+  console.log(weather)
 
   return { city, weather }
 }
