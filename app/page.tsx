a34a40c2b0053f787f2992d6e5b447c587fb78db
Kawashima-Tomoya <104120078+Kawashima-Tomoya@users.noreply.github.com>
2025-01-30 14:45:46 +0900

    🎨

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,10 @@
-/* eslint-disable no-console */
-
-'use client'
-
-import { useState } from 'react'
+import { Form } from '~/components/Form'
 
 export default function Page() {
-  const [city, setCity] = useState('')
-  const [weather, setWeather] = useState('')
-
-  async function getWeather() {
-    const res = await fetch(`/api/weather?city=${city}`)
-    const data = await res.json()
-    console.log(data)
-    setWeather(data.weather.current.weather[0].description)
-  }
-
   return (
     <div className="mt-8 text-center">
-      <input
-        type="text"
-        placeholder="都市名を入力"
-        className="mb-5 mr-3 border p-2"
-        onChange={(e) => {
-          setCity(e.target.value)
-        }}
-        value={city}
-      />
-      <button type="button" className="rounded bg-indigo-600 p-2 text-white" onClick={getWeather}>
-        天気情報を取得
-      </button>
-      <h1>
-        City:
-        {city}
-      </h1>
-      <p>
-        Weather:
-        {weather}
-      </p>
+      <h1 className="mb-4 font-sans text-2xl font-bold">天気</h1>
+      <Form />
     </div>
   )
 }
