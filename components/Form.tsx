93c8fff0538e1270c3e94803f6f773b8aa00a689
Kawashima-Tomoya <104120078+Kawashima-Tomoya@users.noreply.github.com>
2025-02-10 18:03:38 +0900

    ✨ Formコンポーネントの状態管理を改善し、天気データの表示を追加

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,21 +1,28 @@
 'use client'
 
 import { useActionState, useState } from 'react'
+import type { WeatherResponse } from '~/actions/getWeather'
 import { getWeather } from '~/actions/getWeather'
+import { DayWeather } from './DayWeather'
+import { WeatherDetail } from './WeatherDetail'
 
 export function Form() {
-  const [city, setCity] = useState('')
-
-  const initialState = {
-    city: '',
-    weather: '',
+  const initialState: WeatherResponse = {
+    weather: [],
     message: '',
   }
-
-  const [state, formAction, isPending] = useActionState(
+  const [weatherState, formAction, isPending] = useActionState(
     getWeather,
     initialState,
   )
+  const [city, setCity] = useState('')
+  const [selectedDay, setSelectedDay] = useState('')
+
+  const handleDayClick = (date: string) => {
+    setSelectedDay(date)
+  }
+
+  // const selectedDayData = weatherState.find((day: any) => day.dt === selectedDay) || weatherState[0]
 
   return (
     <div className="mt-8 text-center">
@@ -30,18 +37,28 @@ export function Form() {
         />
         <button type="submit" className="rounded bg-indigo-600 p-2 text-white" disabled={isPending}>天気を取得</button>
       </form>
-      <div className="mx-auto mt-5 w-[280px] text-left">
-        <h1 className="">
-          City:&nbsp;
-          {state.city}
-        </h1>
-        <p className="text-left">
-          Weather:&nbsp;
-          {state.weather
-            ? state.weather
-            : <span className="text-left text-sm text-red-400">{state.message}</span>}
-        </p>
-      </div>
+
+      {/* <WeatherDetail data={selectedDayData} /> */}
+      { weatherState
+        ? (
+            <div className="mt-10 grid grid-cols-2 gap-4 sm:grid-cols-4 lg:grid-cols-8">
+              {weatherState.weather.map((day: any) => (
+                <DayWeather
+                  key={day.dt}
+                  data={day}
+                  isSelected={day.dt === selectedDay}
+                  onClick={() => handleDayClick(day.dt)}
+                />
+              ))}
+            </div>
+          )
+        : (
+            isPending
+              ? (
+                  <p className="mt-8 text-xl">Loading...</p>
+                )
+              : null
+          )}
     </div>
   )
 }
